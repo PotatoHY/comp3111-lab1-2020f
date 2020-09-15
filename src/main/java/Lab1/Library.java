0ed4c7c5a565bdf82c2b37cb830a96cbe43a3435
PotatoHY <oscarmaths9141@gmail.com>
2020-09-15 20:11:35 +0800

    Initial commit

---
+++ src/main/java/Lab1/Library.java
@@ -0,0 +1,9 @@
+public class Library {
+	public static void main(String arg[]) {
+		System.out.println("Hello fucking world");
+	}
+	
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
